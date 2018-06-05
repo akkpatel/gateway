3c0b1eb42d793990100a7c09fb376d10e204b710
Anand Patel <>
2018-06-05 16:37:52 -0400

    remove dependency of excessive code.

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -13,9 +13,6 @@ import { NotificationInterceptor } from './blocks/interceptor/notification.inter
 import { GatewaySharedModule, UserRouteAccessService } from './shared';
 import { GatewayAppRoutingModule} from './app-routing.module';
 import { GatewayHomeModule } from './home/home.module';
-import { GatewayAdminModule } from './admin/admin.module';
-import { GatewayAccountModule } from './account/account.module';
-import { GatewayEntityModule } from './entities/entity.module';
 import { PaginationConfig } from './blocks/config/uib-pagination.config';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 import {
@@ -34,9 +31,6 @@ import {
         Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
         GatewaySharedModule,
         GatewayHomeModule,
-        GatewayAdminModule,
-        GatewayAccountModule,
-        GatewayEntityModule,
         // jhipster-needle-angular-add-module JHipster will add new module here
     ],
     declarations: [
